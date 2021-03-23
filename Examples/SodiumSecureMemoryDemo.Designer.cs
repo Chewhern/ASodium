de96fe530c162876b7e3fa6661708c0dd75e3a6c
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-03-23 12:22:14 +0800

    Add files via upload

---
+++ Examples/SodiumSecureMemoryDemo.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LibSodiumBinding
+{
+    partial class SodiumSecureMemoryDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MemZeroBTN = new System.Windows.Forms.Button();
+            this.MemLockUnlockBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MemZeroBTN
+            // 
+            this.MemZeroBTN.Location = new System.Drawing.Point(13, 13);
+            this.MemZeroBTN.Name = "MemZeroBTN";
+            this.MemZeroBTN.Size = new System.Drawing.Size(227, 71);
+            this.MemZeroBTN.TabIndex = 0;
+            this.MemZeroBTN.Text = "Secure Memory Zero";
+            this.MemZeroBTN.UseVisualStyleBackColor = true;
+            this.MemZeroBTN.Click += new System.EventHandler(this.MemZeroBTN_Click);
+            // 
+            // MemLockUnlockBTN
+            // 
+            this.MemLockUnlockBTN.Location = new System.Drawing.Point(13, 102);
+            this.MemLockUnlockBTN.Name = "MemLockUnlockBTN";
+            this.MemLockUnlockBTN.Size = new System.Drawing.Size(227, 71);
+            this.MemLockUnlockBTN.TabIndex = 1;
+            this.MemLockUnlockBTN.Text = "Secure Memory Lock";
+            this.MemLockUnlockBTN.UseVisualStyleBackColor = true;
+            this.MemLockUnlockBTN.Click += new System.EventHandler(this.MemLockBTN_Click);
+            // 
+            // SodiumSecureMemoryDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MemLockUnlockBTN);
+            this.Controls.Add(this.MemZeroBTN);
+            this.Name = "SodiumSecureMemoryDemo";
+            this.Text = "SodiumSecureMemoryDemo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MemZeroBTN;
+        private System.Windows.Forms.Button MemLockUnlockBTN;
+    }
+}
---
+++ Examples/SodiumSecureMemoryDemo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Runtime.InteropServices;
+
+namespace LibSodiumBinding
+{
+    public partial class SodiumSecureMemoryDemo : Form
+    {
+        public SodiumSecureMemoryDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void MemZeroBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] RandomByte = new Byte[32];
+            Byte[] ZeroByte = new Byte[32];
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(RandomByte);
+            //Assume the RandomByte variable is extremely sensitive so you should
+            //clear it from memory is best option
+            //but we have to do it securely
+            //==GCHandle==
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(RandomByte, GCHandleType.Pinned);
+            Sodium.SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), RandomByte.Length);
+            MyGeneralGCHandle.Free();
+            MessageBox.Show(RandomByte.SequenceEqual(ZeroByte).ToString());
+        }
+
+        private void MemLockBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] RandomByte = new Byte[32];
+            Byte[] ZeroByte = new Byte[32];
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(RandomByte);
+            //Assume the RandomByte variable is extremely sensitive so you should
+            //clear it from memory is best option
+            //but we have to do it securely
+            //==GCHandle==
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(RandomByte, GCHandleType.Pinned);
+            Sodium.SodiumSecureMemory.MemLock(MyGeneralGCHandle.AddrOfPinnedObject(), RandomByte.Length);
+            Sodium.SodiumSecureMemory.MemUnlock(MyGeneralGCHandle.AddrOfPinnedObject(), RandomByte.Length);
+            MessageBox.Show(RandomByte.SequenceEqual(ZeroByte).ToString());
+            MyGeneralGCHandle.Free();
+            //Most of the times we use MemLock and MemUnlock in a sequence because by not doing so, you may have encounter memory lock/unlock limits..
+            //MemLock and MemUnlock can be used but they are not as general purpose as Sodium MemZero in my opinion..
+        }
+    }
+}
