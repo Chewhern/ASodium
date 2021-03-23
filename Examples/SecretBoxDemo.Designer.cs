574f3adf0b5d84c8029c001f957f8382c39f6f3d
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-03-23 22:25:51 +0800

    Add files via upload

---
+++ Examples/SecretBoxDemo.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace LibSodiumBinding
+{
+    partial class SecretBoxDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.KeyGenBTN = new System.Windows.Forms.Button();
+            this.NonceGenBTN = new System.Windows.Forms.Button();
+            this.SeededKeyGenBTN = new System.Windows.Forms.Button();
+            this.SeededNonceBTN = new System.Windows.Forms.Button();
+            this.SecretBoxCreateOpenBTN = new System.Windows.Forms.Button();
+            this.DetachedBoxCreateOpenBTN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // KeyGenBTN
+            // 
+            this.KeyGenBTN.Location = new System.Drawing.Point(13, 13);
+            this.KeyGenBTN.Name = "KeyGenBTN";
+            this.KeyGenBTN.Size = new System.Drawing.Size(220, 65);
+            this.KeyGenBTN.TabIndex = 0;
+            this.KeyGenBTN.Text = "Generate Key";
+            this.KeyGenBTN.UseVisualStyleBackColor = true;
+            this.KeyGenBTN.Click += new System.EventHandler(this.KeyGenBTN_Click);
+            // 
+            // NonceGenBTN
+            // 
+            this.NonceGenBTN.Location = new System.Drawing.Point(13, 184);
+            this.NonceGenBTN.Name = "NonceGenBTN";
+            this.NonceGenBTN.Size = new System.Drawing.Size(220, 65);
+            this.NonceGenBTN.TabIndex = 1;
+            this.NonceGenBTN.Text = "Generate Nonce";
+            this.NonceGenBTN.UseVisualStyleBackColor = true;
+            this.NonceGenBTN.Click += new System.EventHandler(this.NonceGenBTN_Click);
+            // 
+            // SeededKeyGenBTN
+            // 
+            this.SeededKeyGenBTN.Location = new System.Drawing.Point(13, 99);
+            this.SeededKeyGenBTN.Name = "SeededKeyGenBTN";
+            this.SeededKeyGenBTN.Size = new System.Drawing.Size(220, 65);
+            this.SeededKeyGenBTN.TabIndex = 2;
+            this.SeededKeyGenBTN.Text = "Generate Seeded Key";
+            this.SeededKeyGenBTN.UseVisualStyleBackColor = true;
+            this.SeededKeyGenBTN.Click += new System.EventHandler(this.SeededKeyGenBTN_Click);
+            // 
+            // SeededNonceBTN
+            // 
+            this.SeededNonceBTN.Location = new System.Drawing.Point(13, 271);
+            this.SeededNonceBTN.Name = "SeededNonceBTN";
+            this.SeededNonceBTN.Size = new System.Drawing.Size(220, 65);
+            this.SeededNonceBTN.TabIndex = 3;
+            this.SeededNonceBTN.Text = "Generate Seeded Nonce";
+            this.SeededNonceBTN.UseVisualStyleBackColor = true;
+            this.SeededNonceBTN.Click += new System.EventHandler(this.SeededNonceBTN_Click);
+            // 
+            // SecretBoxCreateOpenBTN
+            // 
+            this.SecretBoxCreateOpenBTN.Location = new System.Drawing.Point(266, 13);
+            this.SecretBoxCreateOpenBTN.Name = "SecretBoxCreateOpenBTN";
+            this.SecretBoxCreateOpenBTN.Size = new System.Drawing.Size(220, 65);
+            this.SecretBoxCreateOpenBTN.TabIndex = 4;
+            this.SecretBoxCreateOpenBTN.Text = "SecretBox Create/Open";
+            this.SecretBoxCreateOpenBTN.UseVisualStyleBackColor = true;
+            this.SecretBoxCreateOpenBTN.Click += new System.EventHandler(this.SecretBoxCreateOpenBTN_Click);
+            // 
+            // DetachedBoxCreateOpenBTN
+            // 
+            this.DetachedBoxCreateOpenBTN.Location = new System.Drawing.Point(266, 99);
+            this.DetachedBoxCreateOpenBTN.Name = "DetachedBoxCreateOpenBTN";
+            this.DetachedBoxCreateOpenBTN.Size = new System.Drawing.Size(220, 65);
+            this.DetachedBoxCreateOpenBTN.TabIndex = 5;
+            this.DetachedBoxCreateOpenBTN.Text = "Detached Box Create/Open";
+            this.DetachedBoxCreateOpenBTN.UseVisualStyleBackColor = true;
+            this.DetachedBoxCreateOpenBTN.Click += new System.EventHandler(this.DetachedBoxCreateOpenBTN_Click);
+            // 
+            // SecretBoxDemo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 488);
+            this.Controls.Add(this.DetachedBoxCreateOpenBTN);
+            this.Controls.Add(this.SecretBoxCreateOpenBTN);
+            this.Controls.Add(this.SeededNonceBTN);
+            this.Controls.Add(this.SeededKeyGenBTN);
+            this.Controls.Add(this.NonceGenBTN);
+            this.Controls.Add(this.KeyGenBTN);
+            this.Name = "SecretBoxDemo";
+            this.Text = "SecretBoxDemo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button KeyGenBTN;
+        private System.Windows.Forms.Button NonceGenBTN;
+        private System.Windows.Forms.Button SeededKeyGenBTN;
+        private System.Windows.Forms.Button SeededNonceBTN;
+        private System.Windows.Forms.Button SecretBoxCreateOpenBTN;
+        private System.Windows.Forms.Button DetachedBoxCreateOpenBTN;
+    }
+}
---
+++ Examples/SecretBoxDemo.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace LibSodiumBinding
+{
+    public partial class SecretBoxDemo : Form
+    {
+        public SecretBoxDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyGenBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] SecretBoxKey = Sodium.SodiumSecretBox.GenerateKey();
+            MessageBox.Show(new System.Numerics.BigInteger(SecretBoxKey).ToString());
+        }
+
+        private void NonceGenBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] Nonce = Sodium.SodiumSecretBox.GenerateNonce();
+            MessageBox.Show(new System.Numerics.BigInteger(Nonce).ToString());
+        }
+
+        private void SeededKeyGenBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] RandomByte = new Byte[32];
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(RandomByte);
+            Byte[] SeededSecretBoxKey = Sodium.SodiumSecretBox.GenerateSeededKey(RandomByte);
+            MessageBox.Show(new System.Numerics.BigInteger(SeededSecretBoxKey).ToString());
+        }
+
+        private void SeededNonceBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] RandomByte = new Byte[32];
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(RandomByte);
+            Byte[] Nonce = Sodium.SodiumSecretBox.GenerateSeededNonce(RandomByte);
+            MessageBox.Show(new System.Numerics.BigInteger(Nonce).ToString());
+        }
+
+        private void SecretBoxCreateOpenBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] RandomByte = new Byte[32];
+            Byte[] CipherText = new Byte[] { };
+            Byte[] OriginalText = new Byte[] { };
+            Byte[] StreamCipherKey = new Byte[] { };
+            Byte[] Nonce = new Byte[] { };
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(RandomByte);
+            StreamCipherKey = Sodium.SodiumSecretBox.GenerateKey();
+            Nonce = Sodium.SodiumSecretBox.GenerateNonce();
+            CipherText = Sodium.SodiumSecretBox.Create(RandomByte, Nonce, StreamCipherKey);
+            OriginalText = Sodium.SodiumSecretBox.Open(CipherText, Nonce, StreamCipherKey);
+            MessageBox.Show(OriginalText.SequenceEqual(RandomByte).ToString());
+        }
+
+        private void DetachedBoxCreateOpenBTN_Click(object sender, EventArgs e)
+        {
+            Sodium.DetachedBox MyDetachedBox = new Sodium.DetachedBox();
+            Byte[] RandomByte = new Byte[32];
+            Byte[] CipherText = new Byte[] { };
+            Byte[] OriginalText = new Byte[] { };
+            Byte[] OriginalText2 = new Byte[] { };
+            Byte[] StreamCipherKey = new Byte[] { };
+            Byte[] Nonce = new Byte[] { };
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            rngCsp.GetBytes(RandomByte);
+            StreamCipherKey = Sodium.SodiumSecretBox.GenerateKey();
+            Nonce = Sodium.SodiumSecretBox.GenerateNonce();
+            MyDetachedBox = Sodium.SodiumSecretBox.CreateDetached(RandomByte, Nonce, StreamCipherKey);
+            OriginalText=Sodium.SodiumSecretBox.OpenDetached(MyDetachedBox,Nonce,StreamCipherKey);
+            OriginalText2 = Sodium.SodiumSecretBox.OpenDetached(MyDetachedBox.CipherText, MyDetachedBox.Mac, Nonce, StreamCipherKey);
+            MessageBox.Show(RandomByte.SequenceEqual(OriginalText).ToString());
+            MessageBox.Show(RandomByte.SequenceEqual(OriginalText2).ToString());
+        }
+    }
+}
