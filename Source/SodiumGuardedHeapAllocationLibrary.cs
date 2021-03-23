a96e639db76291c474e15e00fd069b18d5eaded4
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-03-23 13:21:28 +0800

    Add files via upload

---
+++ Source/SodiumGuardedHeapAllocationLibrary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Sodium
+{
+    public static partial class SodiumGuardedHeapAllocationLibrary
+    {
+        #if IOS
+            const string DllName = "__Internal";
+        #else
+        const string DllName = "libsodium";
+        #endif
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr sodium_malloc(long Size);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern IntPtr sodium_allocarray(long ArrayLength,long ArrayElementSizeInBytes);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void sodium_free(IntPtr intPtr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int sodium_mprotect_noaccess(IntPtr intPtr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int sodium_mprotect_readonly(IntPtr intPtr);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int sodium_mprotect_readwrite(IntPtr intPtr);
+    }
+}
