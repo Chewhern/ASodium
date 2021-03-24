809f09ae8fff81aecf633f47ae166e10b71c6215
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-03-24 12:16:14 +0800

    Add files via upload

---
+++ Source/SodiumSecretKeyAuth.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Sodium
+{
+    public static class SodiumSecretKeyAuth
+    {
+
+        public static int GetMACLength()
+        {
+            return SodiumSecretKeyAuthLibrary.crypto_auth_bytes();
+        }
+
+        public static int GetKeyLength() 
+        {
+            return SodiumSecretKeyAuthLibrary.crypto_auth_keybytes();
+        }
+
+        public static Byte[] GenKey() 
+        {
+            Byte[] Key = new Byte[GetKeyLength()];
+            
+            SodiumSecretKeyAuthLibrary.crypto_auth_keygen(Key);
+
+            return Key;
+        }
+
+        public static Byte[] Sign(Byte[] message, Byte[] key)
+        {
+            //validate the length of the key
+            if (key == null || key.Length != GetKeyLength())
+                throw new ArgumentException("Error: Key must be "+key.Length+" in length");
+
+            Byte[] MAC = new Byte[GetMACLength()];
+            SodiumSecretKeyAuthLibrary.crypto_auth(MAC, message, message.Length, key);
+
+            return MAC;
+        }
+
+        public static void Verify(Byte[] Message, Byte[] MAC, Byte[] Key)
+        {
+            //validate the length of the key
+            if (Key == null || Key.Length != GetKeyLength())
+                throw new ArgumentException("Error: Key must be " + Key.Length + " in length");
+
+            //validate the length of the MAC
+            if (MAC == null || MAC.Length != GetMACLength())
+                throw new ArgumentException("Error: MAC must be " + MAC.Length + " in length");
+
+            var ret = SodiumSecretKeyAuthLibrary.crypto_auth_verify(MAC, Message, Message.Length, Key);
+
+            if (ret == -1) 
+            {
+                throw new CryptographicException("Error: MAC does not match with Message...");
+            }
+        }
+
+        public static Boolean VerifyMAC(Byte[] Message, Byte[] MAC, Byte[] Key)
+        {
+            //validate the length of the key
+            if (Key == null || Key.Length != GetKeyLength())
+                throw new ArgumentException("Error: Key must be " + Key.Length + " in length");
+
+            //validate the length of the MAC
+            if (MAC == null || MAC.Length != GetMACLength())
+                throw new ArgumentException("Error: MAC must be " + MAC.Length + " in length");
+
+            var ret = SodiumSecretKeyAuthLibrary.crypto_auth_verify(MAC, Message, Message.Length, Key);
+
+            return ret == -1;
+        }
+    }
+}
---
+++ Source/SodiumSecretKeyAuthLibrary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Sodium
+{
+    public static partial class SodiumSecretKeyAuthLibrary
+    {
+        #if IOS
+            const string DllName = "__Internal";
+        #else
+            const string DllName = "libsodium";
+        #endif
+
+        //crypto_auth
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth(Byte[] MAC, Byte[] Message, long MessageLength, Byte[] Key);
+
+        //crypto_auth_keybytes
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_keybytes();
+
+        //crypto_auth_bytes
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_bytes();
+
+        //crypto_auth_verify
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_verify(Byte[] Signature, Byte[] Message, long MessageLength, Byte[] Key);
+
+        //crypto_auth_keygen
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void crypto_auth_keygen(Byte[] Key);
+    }
+}
