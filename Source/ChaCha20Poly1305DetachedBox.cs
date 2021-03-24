fbf796d20f7f8c1fba6b608483b7661dd4b4e1b8
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-03-24 20:36:28 +0800

    Add files via upload

---
+++ Source/ChaCha20Poly1305DetachedBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sodium
+{
+    public class ChaCha20Poly1305DetachedBox
+    {
+        public Byte[] MAC { get; set; }
+
+        public long MACLength { get; set; }
+
+        public Byte[] CipherText { get; set; }
+
+    }
+}
