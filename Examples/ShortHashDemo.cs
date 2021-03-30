3c657dc7d802de62f0b29d2826cbdc65516bfc7f
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-03-30 21:48:07 +0800

    Add files via upload

---
+++ Examples/ShortHashDemo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sodium;
+
+namespace LibSodiumBinding
+{
+    public partial class ShortHashDemo : Form
+    {
+        public ShortHashDemo()
+        {
+            InitializeComponent();
+        }
+
+        private void ComputeHashBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] Key = SodiumShortHash.GenerateKey();
+            Byte[] RandomMessage = SodiumRNG.GetRandomBytes(128);
+            Byte[] ComputedHash = SodiumShortHash.ComputeHash(RandomMessage, Key);
+        }
+
+        private void ComputeHashVariantBTN_Click(object sender, EventArgs e)
+        {
+            Byte[] Key = SodiumShortHash.GenerateKey();
+            Byte[] RandomMessage = SodiumRNG.GetRandomBytes(128);
+            Byte[] ComputedHash = SodiumShortHash.SipHash_2_4ComputeHash(RandomMessage, Key);
+        }
+    }
+}
