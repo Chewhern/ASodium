26a3d76d1a1c5ea97e3d700c413f037c922c97f0
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-04-02 16:19:56 +0800

    Add files via upload

---
+++ Source/SodiumHMACSHA256.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace Sodium
+{
+    public static class SodiumHMACSHA256
+    {
+
+        public static int GetKeyBytesLength() 
+        {
+            return SodiumHMACSHA256Library.crypto_auth_hmacsha256_keybytes();
+        }
+
+        public static int GetComputedMACLength() 
+        {
+            return SodiumHMACSHA256Library.crypto_auth_hmacsha256_bytes();
+        }
+
+        public static Byte[] GenerateKey() 
+        {
+            Byte[] Key = new Byte[GetKeyBytesLength()];
+
+            SodiumHMACSHA256Library.crypto_auth_hmacsha256_keygen(Key);
+
+            return Key;
+        }
+
+        public static IntPtr GenerateKeyIntPtr()
+        {
+            Byte[] Key = new Byte[GetKeyBytesLength()];
+
+            SodiumHMACSHA256Library.crypto_auth_hmacsha256_keygen(Key);
+
+            Boolean IsZero = true;
+            IntPtr KeyIntPtr = SodiumGuardedHeapAllocation.Sodium_Malloc(ref IsZero, GetKeyBytesLength());
+
+            GCHandle MyGeneralGCHandle = new GCHandle();
+
+            if (IsZero == false) 
+            {
+                Marshal.Copy(Key, 0, KeyIntPtr, GetKeyBytesLength());
+                SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(KeyIntPtr);
+                MyGeneralGCHandle = GCHandle.Alloc(Key, GCHandleType.Pinned);
+                SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), Key.LongLength);
+                MyGeneralGCHandle.Free();
+                return KeyIntPtr;
+            }
+            else 
+            {
+                MyGeneralGCHandle = GCHandle.Alloc(Key, GCHandleType.Pinned);
+                SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), Key.LongLength);
+                MyGeneralGCHandle.Free();
+                return IntPtr.Zero;
+            }
+        }
+
+        public static Byte[] ComputeMAC(Byte[] Message,Byte[] Key) 
+        {
+            if (Message == null) 
+            {
+                throw new ArgumentException("Error: Message must not be null");
+            }
+            if (Key == null) 
+            {
+                throw new ArgumentException("Error: Key must not be null");
+            }
+            else 
+            {
+                if (Key.Length != GetKeyBytesLength()) 
+                {
+                    throw new ArgumentException("Error: Key length must exactly be " + GetKeyBytesLength() + " bytes");
+                }
+            }
+            Byte[] ComputedMAC = new Byte[GetComputedMACLength()];
+            int result=SodiumHMACSHA256Library.crypto_auth_hmacsha256(ComputedMAC, Message, Message.LongLength, Key);
+
+            if (result != 0) 
+            {
+                throw new CryptographicException("Failed to compute MAC using HMACSHA256");
+            }
+
+            return ComputedMAC;
+        }
+
+        public static Boolean VerifyMAC(Byte[] MAC,Byte[] Message, Byte[] Key) 
+        {
+            if (Message == null)
+            {
+                throw new ArgumentException("Error: Message must not be null");
+            }
+            if (Key == null)
+            {
+                throw new ArgumentException("Error: Key must not be null");
+            }
+            else
+            {
+                if (Key.Length != GetKeyBytesLength())
+                {
+                    throw new ArgumentException("Error: Key length must exactly be " + GetKeyBytesLength() + " bytes");
+                }
+            }
+            if (MAC == null) 
+            {
+                throw new ArgumentException("Error: MAC must not be null");
+            }
+            else 
+            {
+                if (MAC.Length != GetComputedMACLength()) 
+                {
+                    throw new ArgumentException("Error: MAC length must exactly be " + GetComputedMACLength() + " bytes");
+                }
+            }
+            int result = SodiumHMACSHA256Library.crypto_auth_hmacsha256_verify(MAC, Message, Message.LongLength, Key);
+
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Key, GCHandleType.Pinned);
+            SodiumSecureMemory.MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), Key.LongLength);
+            MyGeneralGCHandle.Free();
+
+            if (result != 0) 
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ Source/SodiumHMACSHA512256Library.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Sodium
+{
+    public static partial class SodiumHMACSHA512256Library
+    {
+        #if IOS
+            const string DllName = "__Internal";
+        #else
+            const string DllName = "libsodium";
+        #endif
+
+        //crypto_auth_hmacsha512256_bytes
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_hmacsha512256_bytes();
+
+        //crypto_auth_hmacsha512256_keybytes
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_hmacsha512256_keybytes();
+
+        //crypto_auth_hmacsha512256
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_hmacsha512256(Byte[] ComputedMAC, Byte[] Message, long MessageLength, Byte[] Key);
+
+        //crypto_auth_hmacsha512256_verify
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_hmacsha512256_verify(Byte[] ComputedMAC, Byte[] Message, long MessageLength, Byte[] Key);
+
+        //crypto_auth_hmacsha512256_keygen
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void crypto_auth_hmacsha512256_keygen(Byte[] Key);
+    }
+}
