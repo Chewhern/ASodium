c59d94f4a5ddd20457a853fa9ace5b3260c47ec3
Chew Lik Hern (Chrono / MrChew ) <angelchew0@gmail.com>
2021-04-11 16:53:29 +0800

    Update SodiumInit.cs

--- Source/SodiumInit.cs
+++ Source/SodiumInit.cs
@@ -27,5 +27,61 @@ namespace ASodium
                 _isInit = true;
             }
         }
+
+        public static void SetEnvironmentVariable(String CustomPath=null) 
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Architecture architecture = RuntimeInformation.OSArchitecture;
+                if (architecture == Architecture.X64)
+                {
+                    if(CustomPath!=null && CustomPath.CompareTo("") != 0) 
+                    {
+                        String OriginalEnvironmentPath = Environment.GetEnvironmentVariable("PATH");
+                        Environment.SetEnvironmentVariable("PATH", OriginalEnvironmentPath + ";" + CustomPath);
+                    }
+                    else 
+                    {
+                        String OriginalEnvironmentPath = Environment.GetEnvironmentVariable("PATH");
+                        String UserName = Environment.UserName;
+                        String LibsodiumDLLPath = @"C:\Users\" + UserName + @"\.nuget\packages\libsodium\";
+                        String LibsodiumDLLVersion = new DirectoryInfo(Directory.GetDirectories(LibsodiumDLLPath)[0]).Name;
+                        String FullLibsodiumDLLPath = LibsodiumDLLPath + LibsodiumDLLVersion + @"\runtimes\win-x64\native\";
+                        Environment.SetEnvironmentVariable("PATH", OriginalEnvironmentPath + ";" + FullLibsodiumDLLPath);
+                    }
+                }
+                else
+                {
+                    if (CustomPath != null && CustomPath.CompareTo("") != 0)
+                    {
+                        String OriginalEnvironmentPath = Environment.GetEnvironmentVariable("PATH");
+                        Environment.SetEnvironmentVariable("PATH", OriginalEnvironmentPath + ";" + CustomPath);
+                    }
+                    else 
+                    {
+                        String OriginalEnvironmentPath = Environment.GetEnvironmentVariable("PATH");
+                        String UserName = Environment.UserName;
+                        String LibsodiumDLLPath = @"C:\Users\" + UserName + @"\.nuget\packages\libsodium\";
+                        String LibsodiumDLLVersion = new DirectoryInfo(Directory.GetDirectories(LibsodiumDLLPath)[0]).Name;
+                        String FullLibsodiumDLLPath = LibsodiumDLLPath + LibsodiumDLLVersion + @"\runtimes\win-x86\native\";
+                        Environment.SetEnvironmentVariable("PATH", OriginalEnvironmentPath + ";" + FullLibsodiumDLLPath);
+                    }
+                }
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                if(CustomPath!=null || CustomPath.CompareTo("") != 0) 
+                {
+                    Environment.SetEnvironmentVariable("PATH", CustomPath);
+                }
+            }
+            else
+            {
+                if (CustomPath != null || CustomPath.CompareTo("") != 0)
+                {
+                    Environment.SetEnvironmentVariable("PATH", CustomPath);
+                }
+            }
+        }
     }
 }
