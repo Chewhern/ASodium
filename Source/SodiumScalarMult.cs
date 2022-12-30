72c0255a5b5c6a3184d5c6560635676962befc6d
Chrono <chrono_legionnaire@protonmail.com>
2022-12-30 10:57:15 +0800

    Add files via upload

---
+++ Source/SodiumScalarMult.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ASodium
+{
+    public static class SodiumScalarMult
+    {
+        public static int Bytes()
+        {
+            return SodiumScalarMultLibrary.crypto_scalarmult_bytes();
+        }
+
+        public static int ScalarBytes()
+        {
+            return SodiumScalarMultLibrary.crypto_scalarmult_scalarbytes();
+        }
+
+        public static Byte Primitive()
+        {
+            return SodiumScalarMultLibrary.crypto_scalarmult_primitive();
+        }
+
+        public static Byte[] Base(Byte[] CurrentUserSecretKey,Boolean ClearKey=false) 
+        {
+            if (CurrentUserSecretKey == null || CurrentUserSecretKey.Length != ScalarBytes())
+                throw new ArgumentException("Error: Secret Key must be " + ScalarBytes() + " in bytes");
+            Byte[] PublicKey = new Byte[ScalarBytes()];
+            SodiumScalarMultLibrary.crypto_scalarmult_base(PublicKey, CurrentUserSecretKey);
+
+            if (ClearKey == true) 
+            {
+                SodiumSecureMemory.SecureClearBytes(CurrentUserSecretKey);
+            }
+
+            return PublicKey;
+        }
+
+        public static Byte[] Mult(Byte[] CurrentUserSecretKey,Byte[] OtherUserPublicKey,Boolean ClearKey=false) 
+        {
+            if (CurrentUserSecretKey == null || CurrentUserSecretKey.Length != ScalarBytes())
+                throw new ArgumentException("Error: Secret Key must be " + ScalarBytes() + " in bytes");
+
+            if (OtherUserPublicKey == null || OtherUserPublicKey.Length != Bytes())
+                throw new ArgumentException("Error: Public Key must be " + Bytes() + " in bytes");
+
+            Byte[] SharedSecret = new Byte[Bytes()];
+            SodiumScalarMultLibrary.crypto_scalarmult(SharedSecret, CurrentUserSecretKey, OtherUserPublicKey);
+
+            if (ClearKey == true) 
+            {
+                SodiumSecureMemory.SecureClearBytes(CurrentUserSecretKey);
+            }
+
+            return SharedSecret;
+        }
+
+        public static IntPtr MultIntPtr(Byte[] CurrentUserSecretKey, Byte[] OtherUserPublicKey, Boolean ClearKey = false)
+        {
+            if (CurrentUserSecretKey == null || CurrentUserSecretKey.Length != ScalarBytes())
+                throw new ArgumentException("Error: Secret Key must be " + ScalarBytes() + " in bytes");
+
+            if (OtherUserPublicKey == null || OtherUserPublicKey.Length != Bytes())
+                throw new ArgumentException("Error: Public Key must be " + Bytes() + " in bytes");
+
+            Byte[] SharedSecret = new Byte[Bytes()];
+            SodiumScalarMultLibrary.crypto_scalarmult(SharedSecret, CurrentUserSecretKey, OtherUserPublicKey);
+
+            Boolean IsZero = true;
+            IntPtr SharedSecretIntPtr = SodiumGuardedHeapAllocation.Sodium_Malloc(ref IsZero,SharedSecret.Length);
+
+            if (ClearKey == true)
+            {
+                SodiumSecureMemory.SecureClearBytes(CurrentUserSecretKey);
+            }
+
+            if (IsZero == false) 
+            {
+                Marshal.Copy(SharedSecret, 0, SharedSecretIntPtr, SharedSecret.Length);
+                SodiumGuardedHeapAllocation.Sodium_MProtect_NoAccess(SharedSecretIntPtr);
+                SodiumSecureMemory.SecureClearBytes(SharedSecret);
+                return SharedSecretIntPtr;
+            }
+            else 
+            {
+                SodiumSecureMemory.SecureClearBytes(SharedSecret);
+                return IntPtr.Zero;
+            }
+        }
+    }
+}
---
+++ Source/SodiumSecretKeyAuthLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ASodium
+{
+    public static partial class SodiumSecretKeyAuthLibrary
+    {
+        #if IOS
+            const string DllName = "__Internal";
+        #else
+            const string DllName = "libsodium";
+        #endif
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth(Byte[] MAC, Byte[] Message, long MessageLength, Byte[] Key);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_keybytes();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_bytes();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_auth_verify(Byte[] Signature, Byte[] Message, long MessageLength, Byte[] Key);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void crypto_auth_keygen(Byte[] Key);
+    }
+}
