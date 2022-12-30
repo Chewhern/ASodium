996250ebc7e42bb9a566bb070ce10f1682bb8626
Chrono <chrono_legionnaire@protonmail.com>
2022-12-30 10:51:17 +0800

    Add files via upload

---
+++ Deprecated Source/0.5.3/RevampedKeyPair.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ASodium
+{
+    public class RevampedKeyPair
+    {
+        private readonly byte[] _publicKey;
+        private readonly byte[] _privateKey;
+
+        /// <summary>Initializes a new instance of the <see cref="KeyPair"/> class.</summary>
+        /// <param name="publicKey">The public key.</param>
+        /// <param name="privateKey">The private key.</param>
+        /// <exception cref="KeyOutOfRangeException"></exception>
+        public RevampedKeyPair(byte[] publicKey, byte[] privateKey)
+        {
+            //verify that the private key length is a multiple of 16
+            if (privateKey.Length % 16 != 0)
+                throw new ArgumentException("Private Key length must be a multiple of 16 bytes.");
+
+            _publicKey = publicKey;
+
+            _privateKey = privateKey;
+            _ProtectKey();
+        }
+
+        ~RevampedKeyPair()
+        {
+            Clear();
+        }
+
+        /// <summary>Gets the Public Key.</summary>
+        public byte[] PublicKey
+        {
+            get { return _publicKey; }
+        }
+
+        /// <summary>Gets the Private Key.</summary>
+        public byte[] PrivateKey
+        {
+            get
+            {
+                _UnprotectKey();
+                var tmp = new byte[_privateKey.Length];
+                Array.Copy(_privateKey, tmp, tmp.Length);
+                _ProtectKey();
+
+                return tmp;
+            }
+        }
+
+        /// <summary>Clear private key and public key through cryptographically secure way.</summary>
+        public void Clear()
+        {
+            SodiumSecureMemory.SecureClearBytes(_privateKey);
+            SodiumSecureMemory.SecureClearBytes(_publicKey);
+        }
+
+        private void _ProtectKey()
+        {
+            #if NET461
+                ProtectedMemory.Protect(_privateKey, MemoryProtectionScope.SameProcess);
+            #endif
+        }
+
+        private void _UnprotectKey()
+        {
+            #if NET461
+                ProtectedMemory.Unprotect(_privateKey, MemoryProtectionScope.SameProcess);
+            #endif
+        }
+    }
+}
---
+++ Deprecated Source/0.5.3/SodiumSecureMemory.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ASodium
+{
+    public static class SodiumSecureMemory
+    {
+        public static void MemZero(IntPtr intPtr, long Length)
+        {
+            SodiumSecureMemoryLibrary.sodium_memzero(intPtr, Length);
+        }
+
+        public static void MemLock(IntPtr intPtr, long Length)
+        {
+            int TestInt;
+            TestInt = SodiumSecureMemoryLibrary.sodium_mlock(intPtr, Length);
+            if (TestInt == -1)
+            {
+                throw new Exception("Exception: Memory requested to lock exceeds the amount of memory that can be locked..");
+            }
+        }
+
+        public static void MemUnlock(IntPtr intPtr, long Length)
+        {
+            int TestInt;
+            TestInt = SodiumSecureMemoryLibrary.sodium_munlock(intPtr, Length);
+            if (TestInt == -1)
+            {
+                throw new Exception("Exception: Unlock and MemZero process failed..");
+            }
+        }
+
+        //Only data types below can use securememory functions
+        //It's recommended to avoid using String
+        //If you can just use Bytes[] or Char[]
+        public static void SecureClearBytes(Byte[] Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), Source.LongLength);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureClearString(String Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureClearCharArray(Char[] Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemZero(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureMemoryLockBytes(Byte[] Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemLock(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureMemoryLockString(String Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemLock(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureMemoryLockCharArray(Char[] Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemLock(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureMemoryUnlockBytes(Byte[] Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemUnlock(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureMemoryUnlockString(String Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemUnlock(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+
+        public static void SecureMemoryUnlockCharArray(Char[] Source)
+        {
+            GCHandle MyGeneralGCHandle = GCHandle.Alloc(Source, GCHandleType.Pinned);
+            MemUnlock(MyGeneralGCHandle.AddrOfPinnedObject(), Source.Length);
+            MyGeneralGCHandle.Free();
+        }
+    }
+}
