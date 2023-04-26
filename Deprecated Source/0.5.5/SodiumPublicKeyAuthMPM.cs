e8ed50fe5cf905a07d9bac5fcb3ffe6e0e6d51d2
Chrono <chrono_legionnaire@protonmail.com>
2023-04-26 13:56:55 +0800

    Add files via upload

---
+++ Deprecated Source/0.5.5/SodiumPublicKeyAuthMPM.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ASodium
+{
+    public static class SodiumPublicKeyAuthMPM
+    {
+        public static int GetStateBytesLength() 
+        {
+            return SodiumPublicKeyAuthLibrary.crypto_sign_statebytes();
+        }
+
+        public static Byte[] InitializeState() 
+        {
+            Byte[] State = new Byte[GetStateBytesLength()];
+
+            int result = SodiumPublicKeyAuthLibrary.crypto_sign_init(State);
+
+            if (result != 0) 
+            {
+                throw new Exception("Error: Failed to initialize state");
+            }
+            return State;
+        }
+
+        public static Byte[] UpdateState(Byte[] OldState,Byte[] Message) 
+        {
+            if (Message == null)
+            {
+                throw new ArgumentException("Error: Message cannot be null");
+            }
+
+            if (OldState == null)
+            {
+                throw new ArgumentException("Error: State cannot be null");
+            }
+            else
+            {
+                if (OldState.LongLength != GetStateBytesLength())
+                {
+                    throw new ArgumentException("Error: State length must be "+GetStateBytesLength()+" bytes in length");
+                }
+            }
+
+            Byte[] NewState = OldState;
+
+            int result = SodiumPublicKeyAuthLibrary.crypto_sign_update(NewState, Message, Message.LongLength);
+
+            if (result != 0) 
+            {
+                throw new Exception("Error: Failed to update state");
+            }
+
+            return NewState;
+        }
+
+        public static Byte[] SignFinalState(Byte[] State,Byte[] SecretKey, Boolean ClearKey = false) 
+        {
+            Byte[] Signature = new Byte[SodiumPublicKeyAuth.GetSignatureBytesLength()];
+            long SignatureLength = 0;
+
+            int result = SodiumPublicKeyAuthLibrary.crypto_sign_final_create(State, Signature, SignatureLength, SecretKey);
+
+            if (result != 0)
+            {
+                throw new CryptographicException("Error: Failed to sign state and create signature");
+            }
+
+            if (ClearKey == true) 
+            {
+                SodiumSecureMemory.SecureClearBytes(SecretKey);
+            }
+
+            return Signature;
+        }
+
+        public static Boolean VerifySignedFinalState(Byte[] State, Byte[] Signature , Byte[] PublicKey)
+        {
+            if (State == null)
+            {
+                throw new ArgumentException("Error: State cannot be null");
+            }
+            else
+            {
+                if (State.LongLength != GetStateBytesLength())
+                {
+                    throw new ArgumentException("Error: State length must be " + GetStateBytesLength() + " bytes in length");
+                }
+            }
+
+            if (Signature == null)
+            {
+                throw new ArgumentException("Error: Signature cannot be null");
+            }
+            else
+            {
+                if (Signature.LongLength != SodiumPublicKeyAuth.GetSignatureBytesLength())
+                {
+                    throw new ArgumentException("Error: Signature length must be " + SodiumPublicKeyAuth.GetSignatureBytesLength() + " bytes in length");
+                }
+            }
+
+            if (PublicKey == null)
+            {
+                throw new ArgumentException("Error: Public Key cannot be null");
+            }
+            else
+            {
+                if (PublicKey.LongLength != SodiumPublicKeyAuth.GetPublicKeyBytesLength())
+                {
+                    throw new ArgumentException("Error: Public Key length must be " + SodiumPublicKeyAuth.GetPublicKeyBytesLength() + " bytes in length");
+                }
+            }
+
+            int result = SodiumPublicKeyAuthLibrary.crypto_sign_final_verify(State, Signature, PublicKey);
+
+            if (result != 0) 
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+        }
+    }
+}
