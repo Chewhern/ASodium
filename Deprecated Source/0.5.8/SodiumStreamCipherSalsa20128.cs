e81b31aba45cee619818e70836b3e98e82a606a6
Chrono <chrono_legionnaire@protonmail.com>
2024-01-06 17:51:18 +0800

    Add files via upload

---
+++ Deprecated Source/0.5.8/SodiumStreamCipherSalsa20128.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ASodium
+{
+    public static class SodiumStreamCipherSalsa20128
+    {
+        public static Byte[] GenerateSalsa20Key() 
+        {
+            return SodiumStreamCipherSalsa20.Salsa20GenerateKey();
+        }
+
+        public static Byte[] GenerateSalsa20Nonce() 
+        {
+            return SodiumStreamCipherSalsa20.GenerateSalsa20Nonce();
+        }
+
+
+        public static Byte[] Salsa2012RoundsEncrypt(Byte[] Message, Byte[] Nonce, Byte[] Key, Boolean ClearKey = false)
+        {
+            if (Message == null)
+            {
+                throw new ArgumentException("Error: Message must not be null");
+            }
+            else
+            {
+                if (Message.Length == 0)
+                {
+                    throw new ArgumentException("Error: Message Length must not be 0");
+                }
+            }
+            if (Nonce == null)
+            {
+                throw new ArgumentException("Error: Nonce must not be null");
+            }
+            else
+            {
+                if (Nonce.Length != SodiumStreamCipherSalsa20.GetSalsa20NonceBytesLength())
+                {
+                    throw new ArgumentException("Error: Nonce Length must exactly be " + SodiumStreamCipherSalsa20.GetSalsa20NonceBytesLength() + " bytes");
+                }
+            }
+            if (Key == null)
+            {
+                throw new ArgumentException("Error: Key must not be null");
+            }
+            else
+            {
+                if (Key.Length != SodiumStreamCipherSalsa20.GetSalsa20KeyBytesLength())
+                {
+                    throw new ArgumentException("Error: Key Length must exactly be " + SodiumStreamCipherSalsa20.GetSalsa20KeyBytesLength() + " bytes");
+                }
+            }
+            Byte[] OutPut = new Byte[Message.LongLength];
+
+            int result = SodiumStreamCipherSalsa20128Library.crypto_stream_salsa2012_xor(OutPut, Message, Message.LongLength, Nonce, Key);
+
+            if (result != 0)
+            {
+                throw new CryptographicException("Failed to encrypt using Salsa20 stream cipher which operates with 12 rounds");
+            }
+
+            if (ClearKey == true)
+            {
+                SodiumSecureMemory.SecureClearBytes(Key);
+            }
+
+            return OutPut;
+        }
+
+        public static Byte[] Salsa2012RoundsDecrypt(Byte[] CipherText, Byte[] Nonce, Byte[] Key, Boolean ClearKey = false)
+        {
+            return Salsa2012RoundsEncrypt(CipherText, Nonce, Key, ClearKey);
+        }
+
+        public static Byte[] Salsa208RoundsDecrypt(Byte[] CipherText, Byte[] Nonce, Byte[] Key, Boolean ClearKey = false)
+        {
+            return Salsa208RoundsEncrypt(CipherText, Nonce, Key,ClearKey);
+        }
+
+        public static Byte[] Salsa208RoundsEncrypt(Byte[] Message, Byte[] Nonce, Byte[] Key, Boolean ClearKey = false)
+        {
+            if (Message == null)
+            {
+                throw new ArgumentException("Error: Message must not be null");
+            }
+            else
+            {
+                if (Message.Length == 0)
+                {
+                    throw new ArgumentException("Error: Message Length must not be 0");
+                }
+            }
+            if (Nonce == null)
+            {
+                throw new ArgumentException("Error: Nonce must not be null");
+            }
+            else
+            {
+                if (Nonce.Length != SodiumStreamCipherSalsa20.GetSalsa20NonceBytesLength())
+                {
+                    throw new ArgumentException("Error: Nonce Length must exactly be " + SodiumStreamCipherSalsa20.GetSalsa20NonceBytesLength() + " bytes");
+                }
+            }
+            if (Key == null)
+            {
+                throw new ArgumentException("Error: Key must not be null");
+            }
+            else
+            {
+                if (Key.Length != SodiumStreamCipherSalsa20.GetSalsa20KeyBytesLength())
+                {
+                    throw new ArgumentException("Error: Key Length must exactly be " + SodiumStreamCipherSalsa20.GetSalsa20KeyBytesLength() + " bytes");
+                }
+            }
+            Byte[] OutPut = new Byte[Message.LongLength];
+
+            int result = SodiumStreamCipherSalsa20128Library.crypto_stream_salsa208_xor(OutPut, Message, Message.LongLength, Nonce, Key);
+
+            if (result != 0)
+            {
+                throw new CryptographicException("Failed to encrypt using Salsa20 stream cipher which operates with 8 rounds");
+            }
+
+            if (ClearKey == true)
+            {
+                SodiumSecureMemory.SecureClearBytes(Key);
+            }
+
+            return OutPut;
+        }
+    }
+}
---
+++ Deprecated Source/0.5.8/SodiumStreamCipherSalsa20128Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ASodium
+{
+    public static partial class SodiumStreamCipherSalsa20128Library
+    {
+        #if IOS
+            const string DllName = "__Internal";
+        #else
+            const string DllName = "libsodium";
+        #endif
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int crypto_stream_salsa2012_xor(Byte[] CipherText, Byte[] Message, long MessageLength, Byte[] Nonce, Byte[] Key);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int crypto_stream_salsa208_xor(Byte[] CipherText, Byte[] Message, long MessageLength, Byte[] Nonce, Byte[] Key);
+    }
+}
